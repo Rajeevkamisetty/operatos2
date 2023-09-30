2fe14597484193664d4bf12a30d1ae995a3f10c9
Rajeevkamisetty <145850864+Rajeevkamisetty@users.noreply.github.com>
2023-09-30 16:04:48 +0530

    Add files via upload

---
+++ 30-09-2023.py
@@ -0,0 +1,124 @@
+#assigment operator
+a= 5
+b = 7
+
+#addition
+print(a + b)
+
+#subration
+print(a - b)
+
+#multiplication
+print(a * b)
+
+# division
+print(a / b)
+
+#module
+print(a % b)
+
+#floor diivision
+print(a//b)
+
+# assignment values
+#equal(=)
+a = "rajeev.17"
+print(a)
+
+# +=
+b = 145
+b += 3
+print(b)
+
+# -=
+b -= 1
+print(b)
+
+# *=
+b *= 8
+print(b)
+
+# /=
+b /= 2.2
+print(b)
+
+# %=
+b %= 5
+print(b)
+
+# //=
+c = 4862
+c //= 7
+print(c)
+
+#logic operators
+
+d = 4
+e = 8
+f = 5
+#and operator
+print(d < e & f < e)
+
+# or operator
+print(d < e | e < f)
+
+# not operator
+print(not(d < e | e < f))
+
+# comparision operator
+x = 1
+v = 5
+# ==
+print(x == v)
+
+# <=
+print(x <= v)
+
+# >=
+print(x >= v)
+s = 4
+k = 5
+
+# !=
+print(s != k)
+
+# <
+print(s < k)
+
+# >
+print(s > k)
+
+#bitwise and operator
+print(s & k)
+
+# bitwise or operator
+print(s | k)
+
+# bitwise xor operator
+print(s ^ k)
+
+# left shift operator
+print(s >> k)
+
+# right shift operator
+print(s << k)
+
+# check adress of a variable id
+a = " "
+print(id(a))
+
+# identity operator
+# is
+print(s is k)
+
+# is not
+print(s is not k)
+
+#membership operations
+# in
+a = "kamiseetyrajeev@gmail.com"
+print("@" in a)
+
+# not in
+a = "kamiseetyrajeev@gmail.com"
+print("@" not in a)
